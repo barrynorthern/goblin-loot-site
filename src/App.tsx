f8dfe1b1eb9e97aba4b375885aa1e0fa40ef7b35
Barry Northern <barry.n@spikeglobal.com>
2022-06-21 11:13:19 +0100

    adding mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './collection.jpg';
+import logo from './collection.svg';
+import { Box, Typography /*, Button*/ } from '@mui/material';
 import './App.css';
-import { useWeb3 } from './Hooks/useWeb3';
+//import { useWeb3 } from './Hooks/useWeb3';
 
 function App() {
   // const { connect } = useWeb3();
 
   return (
-    <div className="App">
+    <Box className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
+        <Typography variant="h1">
           Coming Soon
-        </h1>
-        <h2 className="text-dim">
+        </Typography>
+        <Typography variant="h2" className="text-dim">
           (We iz stil lookin in caves fer fings)
-        </h2>
+        </Typography>
         {/* <button onClick={connect}>Connect</button> */}
       </header>
-    </div>
+    </Box>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import JinkyWoff from './fonts/Jinky.woff'
+import JinkyWoff2 from './fonts/Jinky.woff2';
+
+import { 
+  ThemeProvider,
+  CssBaseline
+} from "@mui/material";
+import {
+  TypographyOptions
+} from "@mui/material/styles/createTypography";
+import {
+  createTheme
+} from "@mui/material";
+
+const typography : TypographyOptions = {
+  fontFamily: 'Jinky,Arial',
+  fontSize: 13.5,
+  button: {
+      letterSpacing: "0.08em",
+      textTransform: "none",
+  },
+  h1: {
+      fontSize: '80px',
+      fontWeight: 500,
+  },
+  h2: {
+    fontSize: '56px',
+      fontWeight: 500,
+  },
+  h5: {
+      fontWeight: 600,
+      fontSize: '1.7rem',
+  },
+  h6: {
+      fontWeight: 400,
+      size: "14px"
+  }
+};
+
+const theme = createTheme({
+  typography: typography,
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Jinky';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 400;
+          src: local('Jinky'), url(${JinkyWoff}) format('woff'),  url(${JinkyWoff2}) format('woff2');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +65,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
